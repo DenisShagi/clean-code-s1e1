1707feda617df9f4fc2155acabad2c343c232d1d
Denis <denis.shagim@icloud.com>
2024-12-03 10:37:15 +0300

    feat: Editing an item in the TODO list

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const buttonAdd = document.querySelector("button");
+const buttonEdit = document.querySelector(".edit");
+const taskList = document.getElementById("incompleteTasks");
 
-const buttonAdd = document.querySelector('button');
+function addTask() {
+  const inputText = document.querySelector(".task").value;
 
-function addTask(){
-    const inputText = document.querySelector('.task').value;
-    const taskList = document.getElementById('incompleteTasks')
-    const newLi = document.createElement('li')
+  const newLi = document.createElement("li");
 
-    const newTaskHTML = `
+  const newTaskHTML = `
      <li>
             <input type='checkbox'>
             <label class="task">${inputText}</label>
@@ -16,10 +17,36 @@ function addTask(){
                 <img src="./remove.svg">
             </button>
         </li>
-    `
+    `;
 
-    newLi.textContent = inputText
-   taskList.insertAdjacentHTML('beforeend', newTaskHTML);
+  newLi.textContent = inputText;
+  if (inputText.trim() === "") {
+    return;
+  }
+  taskList.insertAdjacentHTML("beforeend", newTaskHTML);
 }
 
-buttonAdd.addEventListener('click', addTask)
+function editTask() {}
+
+buttonAdd.addEventListener("click", addTask);
+buttonEdit.addEventListener("click", editTask);
+
+taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit")) {
+    const taskItem = e.target.closest("li");
+    const label = taskItem.querySelector("label");
+    const input = taskItem.querySelector('input[type="text"]');
+    if (taskItem.classList.contains("editMode")) {
+      taskItem.classList.remove("editMode");
+      e.target.textContent = "Edit";
+
+      if (input.value.trim() !== "") {
+        label.textContent = input.value;
+      }
+    } else {
+      taskItem.classList.add("editMode");
+      e.target.textContent = "Save";
+      input.value = label.textContent;
+    }
+  }
+});
