ea288e028aadcba7501afff1d17ca6e1b8126c5c
Denis <denis.shagim@icloud.com>
2024-12-03 09:35:23 +0300

    fix: Fixed the display of items

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ function addTask(){
     const inputText = document.querySelector('.task').value;
     const taskList = document.getElementById('incompleteTasks')
     const newLi = document.createElement('li')
-    console.log(newLi)
+
+    const newTaskHTML = `
+     <li>
+            <input type='checkbox'>
+            <label class="task">${inputText}</label>
+            <input type="text" class="task">
+            <button class="edit">Edit</button>
+            <button class="delete">
+                <img src="./remove.svg">
+            </button>
+        </li>
+    `
+
     newLi.textContent = inputText
-    taskList.appendChild(newLi)
+   taskList.insertAdjacentHTML('beforeend', newTaskHTML);
 }
 
 buttonAdd.addEventListener('click', addTask)
